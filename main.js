455bb11d8fb1401a538ad9721db1699249849c3b
Malo <ndekezi37@gmail.com>
2021-01-26 10:38:20 +0100

    fin de l'exo

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ let h3 = document.getElementsByTagName('h3')
 console.log(h3[0]);
 console.log(h3[1]);
 console.log(h3[2]);
+
+let p = document.getElementsByTagName('p')
+console.log(p);
+
+let li = document.getElementsByTagName('li')
+console.log(li[2]);
